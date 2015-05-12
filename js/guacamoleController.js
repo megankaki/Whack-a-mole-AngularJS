a472c5693a20ebafe0abdfa597212a27e40c8547
Megan Kakitsubo <megankaki@gmail.com>
2015-05-12 15:14:54 -0700

    added function that produces random holesID. in whackMole function, change place of mole using holesID.

--- js/guacamoleController.js
+++ js/guacamoleController.js
@@ -7,17 +7,20 @@ angular
 
         self.score = 0;
         self.holesList = [
-            {moleIshere: false},
-            {moleIshere: false},
-            {moleIshere: false},
-            {moleIshere: false},
-            {moleIshere: false},
-            {moleIshere: false},
-            {moleIshere: false},
-            {moleIshere: false},
-            {moleIshere: true}
+            {moleIsHere: false},
+            {moleIsHere: false},
+            {moleIsHere: false},
+            {moleIsHere: false},
+            {moleIsHere: false},
+            {moleIsHere: false},
+            {moleIsHere: false},
+            {moleIsHere: false},
+            {moleIsHere: true}
         ]
         self.whackMole = whackMole;
+        self.image = "<img src=\'mole.png\'>"
+        self.holesID = holesID;
+
  
 
         // 3 - You might need a variable called self.score that starts at 0
@@ -28,19 +31,35 @@ angular
         //     a) The whackMole function should increment the score
         //     b) ...and make the mole pop up in another random hole (you can use Math.random() just like in the plain JS version of this app)
     
-    // function holesID(){
-    //     return Math.floor(Math.random() * holes.length);
-    // }
+    function holesID(){
+        return Math.floor(Math.random() * self.holesList.length);
+    }
     function whackMole($index){
         alert($index);
         
-        if (self.holesList[$index].moleIshere == true){
+        if (self.holesList[$index].moleIsHere == true){
           alert("You hit a mole!");
           self.score++;
-        } else if (self.holesList[$index].moleIshere == false){
+          self.holesList[$index].moleIsHere = false;
+          self.holesList[holesID()].moleIsHere = true;
+        } else if (self.holesList[$index].moleIsHere == false){
             alert("Nothing is there!");
         }
 
     }
 
-    }
+    // function whackMole($index){
+    //     alert($index);
+        
+    //     if (self.holesList[$index].moleIsHere == true){
+    //       alert("You hit a mole!");
+    //       self.score++;
+    //       self.holesList[$index].moleIsHere = false;
+    //       self.holesList[Math.floor(Math.random() * self.holesList.length)].moleIsHere = true;
+    //     } else if (self.holesList[$index].moleIsHere == false){
+    //         alert("Nothing is there!");
+    //     }
+
+    // }
+
+}
