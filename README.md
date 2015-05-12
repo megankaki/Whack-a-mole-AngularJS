53d656f58cb33eba0e36084108ab86c8733d2cee
Megan Kakitsubo <megankaki@gmail.com>
2015-05-12 13:54:48 -0700

    set array of holesList, put property and values in array, set function called whackMole, set background image based on condition set in array.

---
+++ README.md
@@ -0,0 +1,7 @@
+## Guacamole in Angular
+This is an Angular lab. Feel free to follow steps 1-7 that are referenced in the HTML and JS files.<br>
+There are many ways to create this app. Feel free to follow your own instincts if you'd prefer! Talk to your neighbour too!
+<br>
+Would any of this be relevant to a game of Tic-Tac-Toe?
+<br>
+
---
+++ js/guacamoleController.js
@@ -0,0 +1,45 @@
+angular
+    .module('guacamoleApp')
+    .controller('GuacamoleController', GuacamoleController);
+
+    function GuacamoleController(){
+        var self = this;
+
+        self.score = 0;
+        self.holesList = [
+            {moleIshere: false},
+            {moleIshere: false},
+            {moleIshere: false},
+            {moleIshere: false},
+            {moleIshere: false},
+            {moleIshere: false},
+            {moleIshere: false},
+            {moleIshere: false},
+            {moleIshere: true}
+        ]
+        self.whackMole = whackMole;
+ 
+
+        // 3 - You might need a variable called self.score that starts at 0
+
+        // 4 - You might also need an array called self.holes, which contains 9 hole objects. Each hole object could have a property that says if it contains the mole or not.
+
+        // 7 - You could then write a whackMole function, that runs whenever a hole is clicked (but only if that hole contains the mole!). Remember to write the function and then link it to your controller using self.whackMole = whackMole;
+        //     a) The whackMole function should increment the score
+        //     b) ...and make the mole pop up in another random hole (you can use Math.random() just like in the plain JS version of this app)
+    
+    // function holesID(){
+    //     return Math.floor(Math.random() * holes.length);
+    // }
+    function whackMole($index){
+        alert($index);
+        
+        if (self.holesList[$index].moleIshere == true){
+          alert("You hit a mole!");
+        } else if (self.holesList[$index].moleIshere == false){
+            alert("Nothing is there!");
+        }
+
+    }
+
+    }
